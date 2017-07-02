805353f0476bb82d0889e23dafa2768a50f217ff
Rishabh Tripathi <ristripathi97@gmail.com>
2017-07-03 02:55:55 +0530

    Download Progress

--- main.py
+++ main.py
@@ -15,14 +15,17 @@ def html_source():
     data= BeautifulSoup(req,"html.parser")
     return data
 def img_name():
-    return datetime.now().strftime("%Y%m%d%H%M%S" + ".jpg")
+    return datetime.now().strftime("%Y%m%d%H%M%S.%f" + ".jpg")
 def img_dl(soup):
+    i=0
     for link in soup.findAll("img"):
         img_url = link.get("src")
         if check(img_url):
+            i+=1
             image = urllib.request.urlopen(img_url)
             with open(img_name(),'wb') as f:
                 f.write(image.read())
+                print("Downloaded %d" %i)
 def check(img_url):
     return (img_url.endswith(".jpg") or img_url.endswith(".jpeg")) and (img_url.startswith("http") or img_url.startswith("https"))
 
